7c1cd5b037594f1ba75e8517f23dec4e0a8533eb
Surbhi Singh <sidarora848@gmail.com>
2016-10-02 15:25:12 -0700

    Day 8: Dictionaries and Maps

---
+++ Day 8 Dictionaries and Maps.java
@@ -0,0 +1,26 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> hashmap = new HashMap<String, Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            hashmap.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(hashmap.containsKey(s))
+                System.out.println(s+"="+hashmap.get(s));
+            else
+                System.out.println("Not found");
+        }
+        in.close();
+    }
+}
