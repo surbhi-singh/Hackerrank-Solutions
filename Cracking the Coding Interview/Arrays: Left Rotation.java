4d7a1ebb82556b4ee6e929e87aad88152c4db5be
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-03-04 22:14:01 -0700

    Left Rotation

--- Cracking the Coding Interview/Arrays: Left Rotation.java
+++ Cracking the Coding Interview/Arrays: Left Rotation.java
@@ -7,20 +7,26 @@ import java.util.regex.*;
 public class Solution {
 
     public static int[] arrayLeftRotation(int[] a, int n, int k) {
-        int b[] = new int[n+1];
-        int j=0;
-        for(int i=k;i<n;i++,j++)
+        int temp[] = new int[n];
+        int j = 0;
+        for(int i = 0; i <n; i++)
             {
-            b[j] = a[i];
+            temp[i] = a[i];
         }
-        for(int i=0;i<k;i++,j++)
+        for(int i = k; i < n; i++, j++)
             {
-            b[j] = a[i];
+            a[j] = temp[i];
         }
-        return b;
-      
+        j--;
+        for(int i = 0; i<k; i++)
+            {
+            j++;
+            a[j] = temp[i];
+        }
+        return a;
+
     }
-    
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -29,14 +35,13 @@ public class Solution {
         for(int a_i=0; a_i < n; a_i++){
             a[a_i] = in.nextInt();
         }
-      
+
         int[] output = new int[n];
         output = arrayLeftRotation(a, n, k);
         for(int i = 0; i < n; i++)
             System.out.print(output[i] + " ");
-      
+
         System.out.println();
-      
+
     }
 }
-
