79a19d6028badf4c60ee661c597f60c7dd9b8801
Surbhi Singh <surbhi.chauhan011@gmail.com>
2017-03-04 21:27:35 -0700

    Left Rotation

---
+++ Cracking the Coding Interview/Arrays: Left Rotation.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int[] arrayLeftRotation(int[] a, int n, int k) {
+        int b[] = new int[n+1];
+        int j=0;
+        for(int i=k;i<n;i++,j++)
+            {
+            b[j] = a[i];
+        }
+        for(int i=0;i<k;i++,j++)
+            {
+            b[j] = a[i];
+        }
+        return b;
+      
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+      
+        int[] output = new int[n];
+        output = arrayLeftRotation(a, n, k);
+        for(int i = 0; i < n; i++)
+            System.out.print(output[i] + " ");
+      
+        System.out.println();
+      
+    }
+}
+
